41c0bca73d1dfc17b0e5829765f17d48951a95d5
eugenioenko <eugenioenko@gmail.com>
2020-12-23 09:23:10 -0800

    Added WokString data type

--- data-types.go
+++ data-types.go
@@ -1,29 +1,56 @@
 package main
 
+import (
+	"strconv"
+)
+
 type WokData interface {
-	ToString() (string, bool)
-	ToBoolean() (bool, bool)
-	ToNumber() (int, bool)
-	ToFloat() (float64, bool)
+	ToString() (string, error)
+	ToBoolean() (bool, error)
+	ToInteger() (int64, error)
+	ToFloat() (float64, error)
 	Equals(other WokData) bool
 	GetType() int
 	GetValue() interface{}
 }
 
 const (
-	// parser tokensA
-	WokDataNull   = 0
-	WokDataBool   = 1
-	WokDataNumber = 2
-	WokDataFloat  = 3
-	WokDataString = 4
+	WokDataNull    = 0
+	WokDataBool    = 1
+	WokDataInteger = 2
+	WokDataFloat   = 3
+	WokDataString  = 4
 )
 
-type DataString struct {
+type WokString struct {
 	value string
-	class int
+	dtype int
 }
 
-func (data *DataString) ToString() string {
+func (data *WokString) ToString() (string, error) {
+	return data.value, nil
+}
+
+func (data *WokString) ToBoolean() (bool, error) {
+	return len(data.value) > 0, nil
+}
+
+func (data *WokString) ToInteger() (int64, error) {
+	return strconv.ParseInt(data.value, 10, 64)
+}
+
+func (data *WokString) ToFloat() (float64, error) {
+	return strconv.ParseFloat(data.value, 64)
+}
+
+func (data *WokString) GetType() int {
+	return data.dtype
+}
+
+func (data *WokString) GetValue() string {
 	return data.value
+}
+
+func (data *WokString) Equals(other WokData) bool {
+	return other.GetType() == data.dtype && other.GetValue() == data.value
 }
