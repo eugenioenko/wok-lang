71a34f07fc4a6434245164cc3784997706e517ce
eugenioenko <eugenioenko@gmail.com>
2020-12-23 10:42:21 -0800

    Added WokFloat data type

--- data-types.go
+++ data-types.go
@@ -151,3 +151,42 @@ func (data *WokBoolean) GetValue() interface{} {
 func (data *WokBoolean) Equals(other WokData) bool {
 	return data.GetType() == other.GetType() && data.value == other.GetValue()
 }
+
+type WokFloat struct {
+	value float64
+	dtype int
+}
+
+func NewWokFloat(value float64) *WokFloat {
+	return &WokFloat{value: value, dtype: WokTypeFloat}
+}
+
+func (data *WokFloat) ToString() string {
+	return strconv.FormatFloat(data.value, 'E', -1, 64)
+}
+
+func (data *WokFloat) ToBoolean() bool {
+	return data.value != 0
+}
+
+func (data *WokFloat) ToInteger() int64 {
+	return int64(data.value)
+}
+
+func (data *WokFloat) ToFloat() float64 {
+	return data.value
+}
+
+func (data *WokFloat) GetType() int {
+	return data.dtype
+}
+
+func (data *WokFloat) GetValue() interface{} {
+	return data.value
+}
+
+func (data *WokFloat) Equals(other WokData) bool {
+	// TODO: check if type asertion other.(int64) is faster then interface{} comparison
+	// https://golang.org/ref/spec#Comparison_operators
+	return data.GetType() == other.GetType() && data.value == other.GetValue()
+}
